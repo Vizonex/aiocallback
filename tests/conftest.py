79b89fd5942c6af251888af4b9d67e76a4396f80
Vizonex <114684698+Vizonex@users.noreply.github.com>
2025-06-15 19:28:46 -0500

    EventLoopPolicies are Deprecated in newer versions of python

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,15 +4,15 @@ import sys
 import pytest
 import pytest_asyncio
 
-
-@pytest.fixture(scope="module")
-def event_loop_policy():
-    if sys.platform != "win32":
-        import uvloop  # type:ignore
-        return uvloop.EventLoopPolicy()
-    else:
-        import winloop  # type:ignore
-        return winloop.EventLoopPolicy()
+if sys.version_info < (3, 14):
+    @pytest.fixture(scope="module")
+    def event_loop_policy():
+        if sys.platform != "win32":
+            import uvloop  # type:ignore
+            return uvloop.EventLoopPolicy()
+        else:
+            import winloop  # type:ignore
+            return winloop.EventLoopPolicy()
 
 @pytest_asyncio.fixture(loop_scope="module")
 async def current_loop():
