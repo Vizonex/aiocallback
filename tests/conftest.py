483a59330a62aca8b8a352e66f44a7a5f2424e23
Vizonex <114684698+Vizonex@users.noreply.github.com>
2025-06-26 13:46:12 -0500

    Attempt to support python 3.14

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,27 @@
 import asyncio
 import sys
 import platform
-
 import pytest
 import pytest_asyncio
 
+
 # XXX: PyPy has problems right now so it's also ignored.
-if sys.version_info < (3, 14) and platform.python_implementation() != "PyPy":
+if platform.python_implementation() != "PyPy":
     @pytest.fixture(scope="module")
-    def event_loop_policy():
+    def event_loop():
         if sys.platform != "win32":
             try:
                 import uvloop  # type:ignore
-                return uvloop.EventLoopPolicy()
+                return uvloop.new_event_loop()
             except ModuleNotFoundError: 
-                # pypy fallback
-                return asyncio.DefaultEventLoopPolicy()
+                # fallback
+                return asyncio.new_event_loop()
         else:
             import winloop  # type:ignore
-            return winloop.EventLoopPolicy()
+            return winloop.new_event_loop()
+        
 
 
 @pytest_asyncio.fixture(loop_scope="module")
 async def current_loop():
     return asyncio.get_running_loop()
-
