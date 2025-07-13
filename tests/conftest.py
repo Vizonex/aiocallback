b4ba826fa6fe1d0af225675c216ced6dc1b971a4
Vizonex <VizonexBusiness@gmail.com>
2025-07-13 15:44:19 -0500

    XXX: uvloop breaks when 3.14 is in use

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,24 +11,19 @@ uvloop = pytest.importorskip("winloop" if sys.platform == "win32" else "uvloop")
 if platform.python_implementation() != "PyPy":
     # NOTE: I am working to pytest-asyncio in the future to workaround needing event-loop-policies
 
-    if sys.version_info >= (3, 14):
-        import warnings
-
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore", DeprecationWarning)
-            from asyncio import DefaultEventLoopPolicy
-    else:
+    if sys.version_info <= (3, 14):
         from asyncio import DefaultEventLoopPolicy
 
-    @pytest.fixture(
-        scope="session",
-        params=(
-            DefaultEventLoopPolicy(),
-            uvloop.EventLoopPolicy(),
-        ),
-        ids=str,
-    )
-    def event_loop_policy(
-        request: pytest.FixtureRequest,
-    ) -> asyncio.AbstractEventLoopPolicy:
-        return request.param
+        @pytest.fixture(
+            scope="session",
+            params=(
+                DefaultEventLoopPolicy(),
+                uvloop.EventLoopPolicy(),
+            ),
+            ids=str,
+        )
+        def event_loop_policy(
+            request: pytest.FixtureRequest,
+        ) -> asyncio.AbstractEventLoopPolicy:
+            return request.param
+    
